225fc38e82b895acde7ea7c2ae3924b672a1c816
thiwankadilshanliyanage <79803162+thiwankadilshanliyanage@users.noreply.github.com>
2021-11-10 09:11:42 +0530

    first commit

---
+++ src/Test.java
@@ -0,0 +1,40 @@
+/**
+ *
+ * @author Thiwanka
+ */
+public class Test {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
+class A{
+  int x; 
+
+    public A(int x) {
+        this.x= x;
+    }
+  
+  
+  
+  public int m(){
+      return x+10;
+  }
+}
+class B{
+  int y;
+
+    public B(int y) {
+        this.y = y;
+    }
+  
+  public int n(){
+      return y*2;
+  }
+}
+
+interface Visitor{
+    
+}
